e6161b1554631e587128bd2bf5f38446b42877da
Albert Ogonevskiy <a.ogonevskiy@gmail.com>
2012-04-04 00:34:43 -0400

    indentation changed to 4 spaces

--- azkaban_cleanup.py
+++ azkaban_cleanup.py
@@ -3,126 +3,162 @@ import time
 import datetime
 import sys
 import shutil
-import optparse
-from stat import *
+# import optparse
+from stat import ST_SIZE, ST_MTIME
 
 jobs_dir = '~/apps/jobs'
+logs_dir = '~/apps/logs'
 days_to_keep=7
 backup_dir = '/tmp/azkaban_backup'
 
 def is_execution_file(full_path):
-  """ method checks if file is an Azkaban execution file """
-
-  if not os.path.isfile(full_path): return False
-
-  (name, ext) = os.path.splitext(os.path.split(full_path)[1])
-
-  try:
-    int(name)
-    if ext == '.json': return True
-  except ValueEror:
+    """ method checks if file is an Azkaban execution file """
+    
+    if not os.path.isfile(full_path): return False
+    
+    (name, ext) = os.path.splitext(os.path.split(full_path)[1])
+    
+    try:
+        int(name)
+        if ext == '.json': return True
+    except ValueError:
+        return False
+    
     return False
 
-  return False
-
 def get_execution_files(exec_dir):
-  """ method returns list of execution files (full path) in the specified directory """
-
-  # listing all the files
-  exec_files = os.listdir(exec_dir)
-
-  # filtering execution files
-  exec_files = [os.path.join(exec_dir,f) for f in exec_files if is_execution_file(os.path.join(exec_dir,f))]
-
-  return exec_files
+    """ method returns list of execution files (full path) in the specified directory """
+    
+    # listing all the files
+    exec_files = os.listdir(exec_dir)
+    
+    # filtering execution files
+    exec_files = [os.path.join(exec_dir,f) for f in exec_files if is_execution_file(os.path.join(exec_dir,f))]
+    
+    return exec_files
 
 def init_backup_dirs(root_backup_dir):
-  """ Method initializes (creates) backup directories: root backup dir, executions and logs """
-
-  d = datetime.datetime.now().strftime('azkaban_backup_%Y%m%d_%H%M%S')
-  path = os.path.join(root_backup_dir,d)
-  print 'Backup directory = %s' % path
-
-  logs_backup_path=os.path.join(path,'logs')
-  execs_backup_path=os.path.join(path,'executions')
-  print 'Creating logs backup directory = %s' % logs_backup_path
-  print 'Creating executions backup directory = %s' % execs_backup_path
-  os.makedirs(logs_backup_path)
-  os.makedirs(execs_backup_path)
-  return (logs_backup_path,execs_backup_path)
+    """ Method initializes (creates) backup directories: root backup dir, executions and logs """
+    
+    d = datetime.datetime.now().strftime('azkaban_backup_%Y%m%d_%H%M%S')
+    path = os.path.join(root_backup_dir,d)
+    print 'Backup directory = %s' % path
+    
+    logs_backup_path=os.path.join(path,'logs')
+    execs_backup_path=os.path.join(path,'executions')
+    print 'Creating logs backup directory = %s' % logs_backup_path
+    print 'Creating executions backup directory = %s' % execs_backup_path
+    os.makedirs(logs_backup_path)
+    os.makedirs(execs_backup_path)
+    return (logs_backup_path,execs_backup_path)
 
 
 def backup_old_execution_files(exec_dir, backup_dir, days_to_keep=7):
-  """ method moves execution files that are older than <days_to_keep> to the <backup_dir> """
-  
-  exec_files = get_execution_files(exec_dir)
-  print '# of files in the executions dir = %d' % len(exec_files)
+    """ method moves execution files that are older than <days_to_keep> to the <backup_dir> """
+    
+    exec_files = get_execution_files(exec_dir)
+    print '# of files in the executions dir = %d' % len(exec_files)
+    
+    # getting files older than <days_to_keep>
+    old_files = [f for f in exec_files if (time.time() - os.stat(f)[ST_MTIME]) > days_to_keep * 60 * 60 * 24 ]
+    
+    print '# of old execution files to backup = %d' % len(old_files)
+    
+    # moving files to backup directory
+    for f in old_files:
+        print 'Moving %s to %s' % (f,backup_dir)
+        shutil.move(f,backup_dir)
 
-  # getting files older than <days_to_keep>
-  old_files = [f for f in exec_files if (time.time() - os.stat(f)[ST_MTIME]) > days_to_keep * 60 * 60 * 24 ]
-
-  print '# of old execution files to backup = %d' % len(old_files)
-
-  # moving files to backup directory
-  for f in old_files:
-    print 'Moving %s to %s' % (f,backup_dir)
-    shutil.move(f,backup_dir)
-
-################################
-## Backup empty execution files
-################################
 def backup_empty_execution_files(exec_dir, backup_dir):
-  """ method moves zero sized execution files into the <backup_dir> """
-  exec_files = get_execution_files(exec_dir)
-  print '# of files in the executions dir = %d' % len(exec_files)
+    """ method moves zero sized execution files into the <backup_dir> """
+    
+    exec_files = get_execution_files(exec_dir)
+    print '# of files in the executions dir = %d' % len(exec_files)
+    
+    # gettign files older than <days_to_keep>
+    old_files = [f for f in exec_files if os.stat(f)[ST_SIZE]==0 ]
+    
+    print '# of empty execution files to backup = %d' % len(old_files)
+    
+    # moving files to backup directory
+    for f in old_files:
+        print 'Moving %s to %s' % (f,backup_dir)
+        shutil.move(f,backup_dir)
+    return None
 
-  # gettign files older than <days_to_keep>
-  old_files = [f for f in exec_files if os.stat(f)[ST_SIZE]==0 ]
-
-  print '# of empty execution files to backup = %d' % len(old_files)
-
-  # moving files to backup directory
-  for f in old_files:
-    print 'Moving %s to %s' % (f,backup_dir)
-    shutil.move(f,backup_dir)
-  return None
-
+def backup_old_log_files(logs_dir, backup_dir):
+    
+    # listing all the files
+    job_dirs = os.listdir(logs_dir)
+    
+    # joinng filenames with path
+    job_dirs = [os.path.join(logs_dir, f) for f in job_dirs]
+    
+    # filtering directories
+    job_dirs = [os.path.join(logs_dir,f) for f in job_dirs if os.path.isdir(f)]
+    print '# of job log directories %s' % len(job_dirs)
+    
+    for f in job_dirs:
+        #print f
+#        if len(f) == 24: print 
+        print os.listdir(f)
+    
+    
+    
+    # print job_log_dirs
 
 def main():
 
-  option_parser = optparse.OptionParser()
+#    option_parser = optparse.OptionParser()
+#    
+#    #  p.add_option('--dryRun', '-d') 
+#    option_parser.add_option("-d", "--dryRun", action="store_true", dest="dry_run")
+#    option_parser.add_option("-l", "--logs", dest="logs_dir", help="Azkaban's logs directory", metavar="LOGS_DIR")
+#    option_parser.add_option("-j", "--jobs", dest="jobs_dir", help="Azkaban's jobs directory", metavar="JOBS_DIR")
+#    options, arguments = option_parser.parse_args()
+    
+    if not os.path.exists(backup_dir): 
+        print 'ERROR -- Backup directory does not exist: %s' % backup_dir
+        sys.exit()
+    
+    paths = init_backup_dirs(backup_dir)
 
-#  p.add_option('--dryRun', '-d') 
-  option_parser.add_option("-d", "--dryRun", action="store_true", dest="dry_run")
-  option_parser.add_option("-l", "--logs", dest="logs_dir", help="Azkaban's logs directory", metavar="LOGS_DIR")
-  option_parser.add_option("-j", "--jobs", dest="jobs_dir", help="Azkaban's jobs directory", metavar="JOBS_DIR")
-  options, arguments = option_parser.parse_args()
+    global jobs_dir
+    """ AS SOON AS YOU WRITE TO A VARIABLE, that variable is AUTOMATICALLY considered LOCAL to the function block in which its declared."""        
+    jobs_dir = os.path.expanduser(jobs_dir)
+    print 'Jobs dir = %s' % jobs_dir
 
-  if not os.path.exists(backup_dir): 
-    print 'ERROR -- Backup directory does not exist: %s' % backup_dir
-    sys.exit()
+    global logs_dir
+    logs_dir = os.path.expanduser(logs_dir)
+    print 'Logs dir = %s' % logs_dir
+        
+    exec_dir = os.path.join(jobs_dir, 'executions')
+    print 'Executions dir = %s' % exec_dir
+    
+    print '*** Backing up old execution files - begin ***'
+    backup_old_execution_files(exec_dir, paths[1])
+    print '*** Backing up old execution files - end ***'
+    
+    print '*** Backing up empty execution files - begin ***'
+    backup_empty_execution_files(exec_dir, paths[1])
+    print '*** Backing up empty execution files - end ***'
+    
+    print '*** Backing up old log files - begin ***'
+    backup_old_log_files(logs_dir, paths[0])
+    print '*** Backing up old log files - end ***'
+    
+#    s = "03-09-2012.00.01.07.969"
+#    print s
+#    s = s[0:19]
+#    print s
+#    print time.mktime(time.strptime(s, "%m-%d-%Y.%H.%M.%S"))
+#    print time.time()
 
-  global jobs_dir
-
-  paths = init_backup_dirs(backup_dir)
-
-  jobs_dir = os.path.expanduser(jobs_dir)
-  print 'Jobs dir = %s' % jobs_dir
-
-  exec_dir = os.path.join(jobs_dir, 'executions')
-  print 'Executions dir = %s' % exec_dir
-
-  print '*** Backing up old execution files - begin ***'
-  backup_old_execution_files(exec_dir, paths[1])
-  print '*** Backing up old execution files - end ***'
-
-  print '*** Backing up empty execution files - begin ***'
-  backup_empty_execution_files(exec_dir, paths[1])
-  print '*** Backing up empty execution files - end ***'
 
 # Running
+
 if __name__ == '__main__':
-  main()
+    main()
 #    print is_execution_file('/Users/aogonevskiy/apps/jobs/executions/1.json')
 #    print is_execution_file('/Users/aogonevskiy/apps/jobs/executions/1.exe')
 #    print is_execution_file('/Users/aogonevskiy/apps/jobs/executions/A123.json')
